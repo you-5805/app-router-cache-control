684d1f3a0bebfdd3f6d68b0a89ce5a6d11709f76
Iwamoto Yo <you.iwamoto@prtimes.co.jp>
2023-05-26 17:30:57 +0900

    impl: sample middleware

---
+++ src/_middleware.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from 'next/server';
+import type { Post } from './types/Post';
+import type { NextMiddleware } from 'next/server';
+
+export const config = {
+  matcher: '/posts/:path*',
+};
+
+const middlewareFn = (async (req) => {
+  const res = NextResponse.next();
+
+  const matches = req.nextUrl.pathname.match(/\/posts\/([^?]+)/);
+  if (matches === null) throw new Error();
+
+  const postId = matches[1];
+  const { post }: { post: Post } = await fetch(
+    `http://127.0.0.1:3000/api/posts/${postId}`,
+  ).then((r) => r.json());
+
+  if (post.shareType === 'public') {
+    res.headers.set('Surrogate-Control', 'public, max-age=30');
+  } else {
+    res.headers.set(
+      'Surrogate-Control',
+      'private, no-store, max-age=0, must-revalidate',
+    );
+  }
+
+  return res;
+}) satisfies NextMiddleware;
+
+export default middlewareFn;
